3aa1d16726ac26077ff2602d212de8c2d296d51b
Eric Jensen <ericcj@gmail.com>
2022-11-08 16:27:17 -0500

    fix TIMESTAMP and the netsuite Task.order column

--- src/metabase/driver/netsuite.clj
+++ src/metabase/driver/netsuite.clj
@@ -52,10 +52,21 @@
         (dissoc :host :port :account-id :role-id)
         (finish-fn host port account-id role-id))))
 
+; TIMESTAMP columns (e.g. item.createddate) were causing "Receiver class com.netsuite.jdbc.oabase.oacb does not define or inherit an implementation of the resolved method 'abstract java.lang.Object getObject(int, java.lang.Class)' of interface java.sql.ResultSet"
+; maybe this is a similar concern to how the oracle driver handles TIMESTAMPTZ?
+(defmethod sql-jdbc.execute/read-column-thunk [:netsuite Types/TIMESTAMP]
+  [_ ^ResultSet rs _rsmeta ^Integer i]
+  (fn []
+    (when-let [t (.getTimestamp rs i)]
+      (t/zoned-date-time (t/local-date-time t) (t/zone-id "UTC")))))
+
 ; Hack since SuiteQL doesn't allow quoting of any kind around aliases.
 ; Ideally we'd extend quote-style to support :none or something which then passed :quoted false to hsql/format instead of always passing :quoting as we do now: https://github.com/seancorfield/honeysql#entity-names
+; This is an incomplete solution since you can't use quotes to refer to a subquery's alias either, e.g. you need to unquote "bar" to make this work: select "source"."bar" from (select * from baz) source
 (defmethod driver/mbql->native :netsuite
   [driver outer-query]
   (let [parent-method (get-method driver/mbql->native :oracle)
         compiled      (parent-method driver outer-query)]
-    (assoc compiled :query (str/replace (compiled :query) #" AS \"([^\"]+)\"" " AS $1"))))
+    (assoc compiled :query (str/replace (str/replace (str/replace (str/replace (str/replace (compiled :query) #" AS \"([^\"]+)\"" " AS $1") #"\"([^\"]+)\" AS " "$1 AS ")
+    		; pretty gross to have special-handling for keywords just because of the netsuite "Task"."order" column here, especially avoiding "source".order for limit subquery
+      #"([^e])\"\.order" "$1\".\"order\"") #"AS order([, ])" "AS esc_order$1") #"\.order" ".esc_order"))))
